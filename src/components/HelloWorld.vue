53d2a2543f63a369d64be101c72c0d427d0d2250
ToD <wlunareve@gmail.com>
2021-04-17 00:19:25 +0800

    feat: add swr  library and basic fetch code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,16 +14,66 @@
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  
+  <div v-for="datum in data" :key="datum.id">
+    <span>{{ datum.title }}</span>
+    <span>{{ datum.author }}</span>
+    <span>{{ datum.link }}</span>
+  </div>
+  <button @click="post_articles">add data</button>
+
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
+import { defineProps, reactive, ref,onMounted } from 'vue';
+import useSWRV from 'swrv' 
 
-defineProps({
-  msg: String
-})
+export default {
+  name: 'App',
 
-const state = reactive({ count: 0 })
+  setup (props) {
+    defineProps({
+      msg: String
+    })
+
+    const state = reactive({ 
+      count: 0,
+    })
+
+    const data = ref([])
+
+    const get_articles = fetch('http://localhost:3000/articles')
+      .then(r => r.json())
+      .then(articles => data.value = articles)
+
+    const post_articles = () => {
+      const new_article = {
+        "title": "VS Code for Vue Developers",
+        "author": "Andy",
+        "link": "https://www.vuemastery.com/blog/vs-code-for-vuejs-developers"
+      }
+
+      fetch('http://localhost:3000/articles', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(new_article)
+      })
+
+      data.value = [...data.value, new_article]
+    } 
+
+
+    onMounted(get_articles)
+
+    return {
+      state,
+      data,
+      post_articles,
+    }
+  }
+}
 </script>
 
 <style scoped>
